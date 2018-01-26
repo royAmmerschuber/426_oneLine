642fb5f20817269f6c096901b5ef1137380007cd
royAmmerschuber <roy@ammerschuber.de>
2018-01-26 12:00:51 +0100

    version 4.2R Login Functionallity begin integration to UI

---
+++ src/Control/Register.java
@@ -0,0 +1,14 @@
+package Control;
+
+import java.io.IOException;
+
+@javax.servlet.annotation.WebServlet(name = "Register")
+public class Register extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
