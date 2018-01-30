2ca3587cc6c4089fbfd00f647eb96abc2fa4e0b6
royAmmerschuber <roy@ammerschuber.de>
2018-01-30 09:05:59 +0100

    version 7.0R Register Working completly

--- src/Control/Register.java
+++ src/Control/Register.java
@@ -1,32 +1,52 @@
 package Control;
 
 import javax.servlet.RequestDispatcher;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 
 @javax.servlet.annotation.WebServlet(name = "Register")
 public class Register extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        if(request.getParameter("password1")==request.getParameter("password2")){
-            if(Login.addUser(
-                    request.getParameter("userName"),
-                    request.getParameter("password1"),
-                    request.getParameter("firstName"),
-                    request.getParameter("name"),
-                    request.getParameter("country"),
-                    request.getParameter("city"),
-                    request.getParameter("plz"),
-                    request.getParameter("street"),
-                    request.getParameter("email"),
-                    request.getParameter("phone")
-            )){
-                //request
-            }else{
-                RequestDispatcher rd= request.getRequestDispatcher("/Register.jsp");
-                rd.include(request,response);
-            }
+        BufferedReader r=request.getReader();
+        String l=r.readLine();
+        String[] x=l.split("&");
+        HashMap<String,String> values=new HashMap<>();
+        for (String y:x) {
+            String[] pair=y.split("=");
+            values.put(pair[0],pair[1]);
         }
-        request.getParameter("");
+        
+        //name=test&firstName=test&userName=test&password1=test&password2=test&email=test%40test.cj&phone=test&country=test&city=test&street=test&plz=test
+        if(values.get("password1").equals(values.get("password2"))){
+
+            if(Login.addUser(
+                    values.get("userName"),
+                    values.get("password1"),
+                    values.get("firstName"),
+                    values.get("name"),
+                    values.get("country"),
+                    values.get("city"),
+                    values.get("plz"),
+                    values.get("street"),
+                    values.get("email"),
+                    values.get("phone")
+            )){
+                response.sendRedirect("/Auth/Login");
+                return;
+            }else{
+                request.getRequestDispatcher("/Register.jsp").forward(request,response);
+
+            }
+
+
+        }else{
+            request.getRequestDispatcher("/Register.jsp").forward(request,response);
+
+        }
+        request.getRequestDispatcher("/Register.jsp").forward(request,response);
+
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
---
+++ src/Control/SLogin.java
@@ -0,0 +1,35 @@
+package Control;
+
+import javax.jms.Session;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.HashMap;
+
+@WebServlet(name = "SLogin")
+public class SLogin extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HashMap<String,String> values=new HashMap<>();
+        for (String x:request.getReader().readLine().split("&")) {
+            String[] pair=x.split("=");
+            values.put(pair[0],pair[1]);
+        }
+        //---------------------------------------------------------------
+        /*if(Login.check(values.get("username"),values.get("pass"))){
+
+            HttpSession s=request.getSession();
+            s.setAttribute("name",values.get("username"));
+            response.sendRedirect("/Shop/Main");
+        }else{
+            request.getRequestDispatcher("/login.jsp").forward(request,response);
+        }*/
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/login.jsp").forward(request,response);
+    }
+}
